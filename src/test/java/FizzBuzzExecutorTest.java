636ca8b8932e7996510b65bb1edf0a999732a4fa
SashavanRee <150018652+SashavanRee@users.noreply.github.com>
2025-02-10 22:24:10 +0100

    Opdracht 1 t/m 3

---
+++ src/test/java/FizzBuzzExecutorTest.java
@@ -0,0 +1,20 @@
+import nl.han.ica.oose.dea.testedfizzbuzz.FizzBuzzExecutor;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FizzBuzzExecutorTest {
+
+    @Test
+    void executeWithValidIntTest(){
+        // Arrange
+        var sut = new FizzBuzzExecutor();
+
+        // Act
+        var testValue = sut.execute(37);
+
+        // Assert
+        Assertions.assertEquals("37", testValue);
+    }
+}
